81466ddff55ac601269c5844a9d48e9376b4f323
Spaedr config <myconfig@example.com>
2026-08-22 13:14:51 +0330

    Add installer

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "=============================="
+echo "      Spaedr config"
+echo "=============================="
+echo "در حال دریافت پروژه..."
+mkdir -p "$HOME/Spaedr-config"
+echo "Spaedr config آماده شد."
